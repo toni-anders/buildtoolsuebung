58b9a69d6d90e1c58c877ec90798ec219fe43738
Toni <Toni@172.16.25.207>
2015-11-05 13:14:49 +0100

    Tests success

--- src/test/java/fhb/AppIT.java
+++ src/test/java/fhb/AppIT.java
@@ -7,15 +7,15 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
+public class AppIT  extends TestCase
 {
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
+
+    public AppIT(String testName )
     {
         super( testName );
     }
@@ -25,7 +25,7 @@ public class AppTest
      */
     public static Test suite()
     {
-        return new TestSuite( AppTest.class );
+        return new TestSuite( AppIT.class );
     }
 
     /**
